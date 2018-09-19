a4ec2f8103116cd3313d24f3f09fd8ae04b7a731
karthisivam <42237459+karthisivam@users.noreply.github.com>
2018-09-19 19:48:15 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,8 @@
+a = 20
+b = 30
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
---
+++ tuple.py
@@ -0,0 +1,10 @@
+tuple1 = ('maths','science',1999,2000)
+tuple2 = (1,2,3,4,5,6,7)
+print ("tuple1(0): ",tuple1[0])
+print ("tuple2(2:5): ",tuple2[2:5])
+
+list1 = ('physics','science',2000,2001)
+list2 = ('1,2,3,4,5,6,7)
+
+print ("list1(0): ",list1[0])
+print ("list2(3:5): ",list2[[3:5])
